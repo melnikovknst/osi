fbc22d7103638ef9ed971d78d846b1f24e77672c
melnikovknst <melnikovknst@gmail.com>
2025-10-28 02:03:33 +0700

    segfault fixed (need to add cleaner thr killing logic & fix joinable ret val printing)

--- 1.6/mythread.c
+++ 1.6/mythread.c
@@ -1,5 +1,6 @@
 #define _GNU_SOURCE
 #include "mythread.h"
+
 #include <sched.h>
 #include <sys/mman.h>
 #include <sys/syscall.h>
@@ -8,6 +9,7 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <stdint.h>
+#include <string.h>
 
 struct start_pack {
     void *(*fn)(void *);
@@ -16,10 +18,10 @@ struct start_pack {
 };
 
 struct watch {
-    int *ctid;
-    void *stack;
-    size_t stack_sz;
-    void *pack;
+    int    *ctid;
+    void   *stack;
+    size_t  stack_sz;
+    void   *pack;
     struct watch *next;
 };
 
@@ -27,47 +29,55 @@ static struct {
     int init;
     int lock;
     struct watch *head;
-    void *stack;
+    void  *stack;
     size_t stack_sz;
     int tid;
 } cleaner_struct;
 
 static int tramp(void *p) {
     struct start_pack *sp = (struct start_pack *)p;
-    void *rv = sp->fn(sp->arg);
-    if (sp->ret_slot) {
+
+    void *rv = NULL;
+    if (sp && sp->fn)
+        rv = sp->fn(sp->arg);
+
+    if (sp && sp->ret_slot)
         *sp->ret_slot = rv;
-    }
+
     syscall(SYS_exit, 0);
-    return 0;
 }
 
 int mythread_create(mythread_t *thr, void *(*start_routine)(void *), void *arg) {
-    const size_t stk_sz = 1<<20;
-    void *stk = mmap(NULL, stk_sz, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS|MAP_STACK, -1, 0);
-    if (stk == MAP_FAILED) {
+    if (!thr || !start_routine) {
+        errno = EINVAL;
         return -1;
     }
 
-    int *ctid = malloc(sizeof *ctid);
-    if (!ctid) { 
-        int e = errno; 
-        munmap(stk, stk_sz); 
-        errno = e; 
-        return -1; 
-    }
+    const size_t stk_sz = 1 << 20;
+    void *stk = mmap(NULL, stk_sz,
+                     PROT_READ | PROT_WRITE,
+                     MAP_PRIVATE | MAP_ANONYMOUS | MAP_STACK,
+                     -1, 0);
+    if (stk == MAP_FAILED)
+        return -1;
 
+    int *ctid = (int*)malloc(sizeof *ctid);
+    if (!ctid) {
+        int e = errno;
+        munmap(stk, stk_sz);
+        errno = e;
+        return -1;
+    }
     *ctid = 0;
 
-    struct start_pack *sp = (struct start_pack*)malloc(sizeof(*sp));
-    if (!sp) { 
-        int e = errno; 
-        munmap(stk, stk_sz); 
-        free(ctid); 
-        errno = e; 
-        return -1; 
+    struct start_pack *sp = (struct start_pack*)malloc(sizeof *sp);
+    if (!sp) {
+        int e = errno;
+        munmap(stk, stk_sz);
+        free(ctid);
+        errno = e;
+        return -1;
     }
-
     sp->fn = start_routine;
     sp->arg = arg;
     sp->ret_slot = &thr->retval;
@@ -76,7 +86,8 @@ int mythread_create(mythread_t *thr, void *(*start_routine)(void *), void *arg) 
               | CLONE_SYSVSEM | CLONE_THREAD
               | CLONE_CHILD_CLEARTID | CLONE_PARENT_SETTID | CLONE_CHILD_SETTID;
 
-    int tid = syscall(SYS_clone, flags, (char*)stk + stk_sz, &thr->tid, ctid, NULL);
+    int tid = clone(tramp, (char*)stk + stk_sz, flags, sp, &thr->tid, NULL, ctid);
+
     if (tid == -1) {
         int e = errno;
         munmap(stk, stk_sz);
@@ -86,11 +97,7 @@ int mythread_create(mythread_t *thr, void *(*start_routine)(void *), void *arg) 
         return -1;
     }
 
-    if (tid == 0) {
-        (void)tramp(sp);
-        return 0;
-    }
-
+    *ctid = tid;
     thr->tid = tid;
     thr->stack = stk;
     thr->stack_sz = stk_sz;
@@ -103,22 +110,17 @@ int mythread_create(mythread_t *thr, void *(*start_routine)(void *), void *arg) 
 }
 
 int mythread_join(mythread_t *thr, void **retval) {
-    if (!thr || thr->detached) { 
-        errno = EINVAL; 
+    if (!thr || thr->detached) {
+        errno = EINVAL;
         return -1;
     }
-
     while (1) {
         int v = *thr->ctid;
-        if (v == 0) {
-            break;
-        }
+        if (v == 0) break;
         syscall(SYS_futex, thr->ctid, FUTEX_WAIT, v, NULL, NULL, 0);
     }
-
-    if (retval) {
+    if (retval)
         *retval = thr->retval;
-    }
 
     munmap(thr->stack, thr->stack_sz);
     free(thr->ctid);
@@ -130,15 +132,15 @@ int mythread_join(mythread_t *thr, void **retval) {
     thr->pack = NULL;
     thr->tid = 0;
     thr->retval = NULL;
+    thr->detached = 1;
 
     return 0;
 }
 
 static int lock_acq(int *l) {
     while (1) {
-        if (__sync_bool_compare_and_swap(l, 0, 1)) {
+        if (__sync_bool_compare_and_swap(l, 0, 1))
             return 0;
-        }
         syscall(SYS_futex, l, FUTEX_WAIT, 1, NULL, NULL, 0);
     }
 }
@@ -169,82 +171,71 @@ static int cleaner(void *arg) {
         }
         lock_rel(&cleaner_struct.lock);
     }
-
-    return 0;
+    return -1;
 }
 
 static int cleaner_start(void) {
-    if (cleaner_struct.init) {
+    if (cleaner_struct.init)
         return 0;
-    }
 
-    cleaner_struct.stack_sz = 1<<20;
-    cleaner_struct.stack = mmap(NULL, cleaner_struct.stack_sz, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS|MAP_STACK, -1, 0);
-    if (cleaner_struct.stack == MAP_FAILED) {
+    cleaner_struct.stack_sz = 1 << 20;
+    cleaner_struct.stack = mmap(NULL, cleaner_struct.stack_sz, PROT_READ | PROT_WRITE, 
+        MAP_PRIVATE | MAP_ANONYMOUS | MAP_STACK, -1, 0);
+    if (cleaner_struct.stack == MAP_FAILED)
         return -1;
-    }
 
-    int flags = CLONE_VM | CLONE_FS | CLONE_FILES | CLONE_SIGHAND | CLONE_SYSVSEM | CLONE_THREAD;
+    int flags = CLONE_VM | CLONE_FS | CLONE_FILES |
+                CLONE_SIGHAND | CLONE_SYSVSEM | CLONE_THREAD;
+
     int tid = clone(cleaner, (char*)cleaner_struct.stack + cleaner_struct.stack_sz, flags, NULL);
 
     if (tid == -1) {
         int e = errno;
         munmap(cleaner_struct.stack, cleaner_struct.stack_sz);
         errno = e;
-        return -1; 
+        return -1;
     }
-
     cleaner_struct.tid = tid;
     cleaner_struct.init = 1;
-
     return 0;
 }
 
 int mythread_detach(mythread_t *thr) {
-    if (!thr) { 
-        errno = EINVAL; 
-        return -1; 
+    if (!thr) {
+        errno = EINVAL;
+        return -1;
     }
-
-    if (thr->detached) {
+    if (thr->detached)
         return 0;
-    }
 
     thr->detached = 1;
+
+    if (thr->pack) {
+        ((struct start_pack*)thr->pack)->ret_slot = NULL;
+    }
 
     if (*thr->ctid == 0) {
         munmap(thr->stack, thr->stack_sz);
         free(thr->ctid);
         free(thr->pack);
+    } else {
+        if (cleaner_start() == -1)
+            return -1;
 
-        thr->stack = NULL;
-        thr->stack_sz = 0;
-        thr->ctid = NULL;
-        thr->pack = NULL;
-        thr->tid = 0;
-        thr->retval = NULL;
+        struct watch *w = (struct watch*)malloc(sizeof *w);
+        if (!w)
+            return -1;
 
-        return 0;
+        w->ctid = thr->ctid;
+        w->stack = thr->stack;
+        w->stack_sz = thr->stack_sz;
+        w->pack = thr->pack;
+
+        lock_acq(&cleaner_struct.lock);
+        w->next = cleaner_struct.head;
+        cleaner_struct.head = w;
+        lock_rel(&cleaner_struct.lock);
     }
-
-    if (cleaner_start() == -1) {
-        return -1;
-    }
-
-    struct watch *w = (struct watch*)malloc(sizeof(*w));
-    if (!w) {
-        return -1;
-    }
-
-    w->ctid = thr->ctid;
-    w->stack = thr->stack;
-    w->stack_sz = thr->stack_sz;
-    w->pack = thr->pack;
-
-    lock_acq(&cleaner_struct.lock);
-    w->next = cleaner_struct.head;
-    cleaner_struct.head = w;
-    lock_rel(&cleaner_struct.lock);
 
     thr->stack = NULL;
     thr->stack_sz = 0;
