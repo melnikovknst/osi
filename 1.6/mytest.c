11b4f02cbdd8f4d22e685529fb80812bac3aae83
melnikovknst <melnikovknst@gmail.com>
2025-10-27 15:29:47 +0700

    add test func

--- 1.6/mytest.c
+++ 1.6/mytest.c
@@ -3,21 +3,45 @@
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
+#include <stdint.h>
+#include <sys/syscall.h>
 #include "mythread.h"
 
-static void *work(void *arg) {
+static void *worker_join(void *arg) {
+    int x = (int)(intptr_t)arg;
+    x++;
+    return (void*)(intptr_t)x;
+}
+
+static void *worker_detach(void *arg) {
     (void)arg;
-    write(1, "hello from mythread\n", 21);
+    sleep(5);
+    write(1, "detached thr: done\n", 22);
     return NULL;
 }
 
 int main(void) {
-    mythread_t t;
-    int err = mythread_create(&t, work, NULL);
-    if (err) {
-        fprintf(stderr, "mythread_create: %s\n", strerror(errno));
+    mythread_t tj, td;
+    void *ret = NULL;
+
+    if (mythread_create(&tj, worker_join, (void*)(intptr_t)5)) {
+        fprintf(stderr, "create(join): %s\n", strerror(errno));
         return 1;
     }
-    sleep(1);
-    return 0;
+    if (mythread_create(&td, worker_detach, NULL)) {
+        fprintf(stderr, "create(detach): %s\n", strerror(errno));
+        return 1;
+    }
+    if (mythread_detach(&td)) {
+        fprintf(stderr, "detach: %s\n", strerror(errno));
+        return 1;
+    }
+
+    if (mythread_join(&tj, &ret)) {
+        fprintf(stderr, "join: %s\n", strerror(errno));
+        return 1;
+    }
+    printf("join ret val = %d\n", (int)(intptr_t)ret);
+
+    syscall(SYS_exit, 0);
 }
