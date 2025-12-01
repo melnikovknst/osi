ad1ee2e3b5b5192694470bb05839523c3e770d1e
melnikovknst <melnikovknst@gmail.com>
2025-12-01 23:18:12 +0700

    spinlock done

---
+++ spinlock/list.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#define _GNU_SOURCE
+#include <pthread.h>
+
+typedef struct _Node {
+    char value[100];
+    struct _Node *next;
+    pthread_spinlock_t sync;
+} Node;
+
+typedef struct _Storage {
+    Node *first;
+} Storage;
+
+Storage* storage_init(int n);
+void storage_destroy(Storage *st);
