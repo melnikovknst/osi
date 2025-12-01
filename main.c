7522bcab713dac323ecbc567b4bed5c511afad2c
melnikovknst <melnikovknst@gmail.com>
2025-12-01 22:15:57 +0700

    refactored

--- main.c
+++ main.c
@@ -30,11 +30,12 @@ static _Atomic long swaps_inc = 0;
 static _Atomic long swaps_dec = 0;
 static _Atomic long swaps_eq = 0;
 
-static void *worker_thread(void *arg) {
+static int list_size = 0;
+
+static void *counter_thread(void *arg) {
 	worker_arg_t *warg = (worker_arg_t *)arg;
 	Storage *st = warg->st;
 	int type = warg->type;
-	unsigned int seed = (unsigned int)(time(NULL) ^ gettid());
 
 	while (1) {
 		long local_pairs = 0;
@@ -50,10 +51,10 @@ static void *worker_thread(void *arg) {
 			continue;
 		}
 
-		pthread_mutex_lock(&cur->sync);
-
 		while (1) {
+			pthread_mutex_lock(&cur->sync);
 			Node *next = cur->next;
+
 			if (!next) {
 				pthread_mutex_unlock(&cur->sync);
 				break;
@@ -71,40 +72,9 @@ static void *worker_thread(void *arg) {
 			else if (type == WORKER_EQ && len1 == len2)
 				local_pairs++;
 
-			Node *third = next->next;
-            if (third && rand_r(&seed) % 2 == 0) {
-                pthread_mutex_lock(&third->sync);
-
-                int len3 = (int)strlen(third->value);
-                int need_swap = 0;
-
-                if (type == WORKER_INC && len2 > len3)
-                    need_swap = 1;
-                else if (type == WORKER_DEC && len2 < len3)
-                    need_swap = 1;
-                else if (type == WORKER_EQ && len2 == len3)
-                    need_swap = 1;
-
-                if (need_swap) {
-                    Node *tail = third->next;
-                    cur->next = third;
-                    third->next = next;
-                    next->next = tail;
-
-                    if (type == WORKER_INC)
-                        atomic_fetch_add(&swaps_inc, 1);
-                    else if (type == WORKER_DEC)
-                        atomic_fetch_add(&swaps_dec, 1);
-                    else
-                        atomic_fetch_add(&swaps_eq, 1);
-                }
-
-                pthread_mutex_unlock(&third->sync);
-            }
-        
-
 			pthread_mutex_unlock(&cur->sync);
-            pthread_mutex_unlock(&next->sync);
+			pthread_mutex_unlock(&next->sync);
+
 			cur = next;
 		}
 
@@ -119,7 +89,83 @@ static void *worker_thread(void *arg) {
 	return NULL;
 }
 
-static void *monitor_thread() {
+static void *swap_thread(void *arg) {
+	worker_arg_t *warg = (worker_arg_t *)arg;
+	Storage *st = warg->st;
+	int type = warg->type;
+	unsigned int seed = (unsigned int)(time(NULL) ^ gettid());
+
+	while (1) {
+		if (list_size < 3)
+			continue;
+
+		int idx = rand_r(&seed) % (list_size - 2);
+
+		Node *prev = st->first;
+		int pos = 0;
+
+		while (prev && pos < idx) {
+			prev = prev->next;
+			pos++;
+		}
+
+		if (!prev)
+			continue;
+
+		Node *cur = prev->next;
+		if (!cur)
+			continue;
+
+		Node *next = cur->next;
+		if (!next)
+			continue;
+
+		pthread_mutex_lock(&prev->sync);
+		pthread_mutex_lock(&cur->sync);
+		pthread_mutex_lock(&next->sync);
+
+		if (prev->next != cur || cur->next != next) {
+			pthread_mutex_unlock(&next->sync);
+			pthread_mutex_unlock(&cur->sync);
+			pthread_mutex_unlock(&prev->sync);
+			continue;
+		}
+
+		int len2 = (int)strlen(cur->value);
+		int len3 = (int)strlen(next->value);
+		int need_swap = 0;
+
+		if (type == WORKER_INC && len2 > len3)
+			need_swap = 1;
+		else if (type == WORKER_DEC && len2 < len3)
+			need_swap = 1;
+		else if (type == WORKER_EQ && len2 == len3)
+			need_swap = 1;
+
+		if (need_swap) {
+			Node *tail = next->next;
+			prev->next = next;
+			next->next = cur;
+			cur->next = tail;
+
+			if (type == WORKER_INC)
+				atomic_fetch_add(&swaps_inc, 1);
+			else if (type == WORKER_DEC)
+				atomic_fetch_add(&swaps_dec, 1);
+			else
+				atomic_fetch_add(&swaps_eq, 1);
+		}
+
+		pthread_mutex_unlock(&next->sync);
+		pthread_mutex_unlock(&cur->sync);
+		pthread_mutex_unlock(&prev->sync);
+	}
+
+	return NULL;
+}
+
+static void *monitor_thread(void *arg) {
+	(void)arg;
 
 	while (1) {
 		sleep(1);
@@ -142,42 +188,77 @@ static void *monitor_thread() {
 int main() {
 	int n = 1000;
 	int err;
-
-	srand(time(NULL));
+	list_size = n;
 
 	Storage *st = storage_init(n);
 
-	pthread_t tid_inc;
-	pthread_t tid_dec;
-	pthread_t tid_eq;
+	pthread_t tid_counter_inc;
+	pthread_t tid_counter_dec;
+	pthread_t tid_counter_eq;
+
+	pthread_t tid_swap_inc;
+	pthread_t tid_swap_dec;
+	pthread_t tid_swap_eq;
+
 	pthread_t tid_mon;
 
-	worker_arg_t arg_inc;
-	worker_arg_t arg_dec;
-	worker_arg_t arg_eq;
+	worker_arg_t arg_counter_inc;
+	worker_arg_t arg_counter_dec;
+	worker_arg_t arg_counter_eq;
 
-	arg_inc.st = st;
-	arg_inc.type = WORKER_INC;
+	worker_arg_t arg_swap_inc;
+	worker_arg_t arg_swap_dec;
+	worker_arg_t arg_swap_eq;
 
-	arg_dec.st = st;
-	arg_dec.type = WORKER_DEC;
+	arg_counter_inc.st = st;
+	arg_counter_inc.type = WORKER_INC;
 
-	arg_eq.st = st;
-	arg_eq.type = WORKER_EQ;
+	arg_counter_dec.st = st;
+	arg_counter_dec.type = WORKER_DEC;
 
-	err = pthread_create(&tid_inc, NULL, worker_thread, &arg_inc);
+	arg_counter_eq.st = st;
+	arg_counter_eq.type = WORKER_EQ;
+
+	arg_swap_inc.st = st;
+	arg_swap_inc.type = WORKER_INC;
+
+	arg_swap_dec.st = st;
+	arg_swap_dec.type = WORKER_DEC;
+
+	arg_swap_eq.st = st;
+	arg_swap_eq.type = WORKER_EQ;
+
+	err = pthread_create(&tid_counter_inc, NULL, counter_thread, &arg_counter_inc);
 	if (err) {
 		printf("main: pthread_create() failed: %s\n", strerror(err));
 		abort();
 	}
 
-	err = pthread_create(&tid_dec, NULL, worker_thread, &arg_dec);
+	err = pthread_create(&tid_counter_dec, NULL, counter_thread, &arg_counter_dec);
 	if (err) {
 		printf("main: pthread_create() failed: %s\n", strerror(err));
 		abort();
 	}
 
-	err = pthread_create(&tid_eq, NULL, worker_thread, &arg_eq);
+	err = pthread_create(&tid_counter_eq, NULL, counter_thread, &arg_counter_eq);
+	if (err) {
+		printf("main: pthread_create() failed: %s\n", strerror(err));
+		abort();
+	}
+
+	err = pthread_create(&tid_swap_inc, NULL, swap_thread, &arg_swap_inc);
+	if (err) {
+		printf("main: pthread_create() failed: %s\n", strerror(err));
+		abort();
+	}
+
+	err = pthread_create(&tid_swap_dec, NULL, swap_thread, &arg_swap_dec);
+	if (err) {
+		printf("main: pthread_create() failed: %s\n", strerror(err));
+		abort();
+	}
+
+	err = pthread_create(&tid_swap_eq, NULL, swap_thread, &arg_swap_eq);
 	if (err) {
 		printf("main: pthread_create() failed: %s\n", strerror(err));
 		abort();
@@ -189,9 +270,12 @@ int main() {
 		abort();
 	}
 
-	pthread_join(tid_inc, NULL);
-	pthread_join(tid_dec, NULL);
-	pthread_join(tid_eq, NULL);
+	pthread_join(tid_counter_inc, NULL);
+	pthread_join(tid_counter_dec, NULL);
+	pthread_join(tid_counter_eq, NULL);
+	pthread_join(tid_swap_inc, NULL);
+	pthread_join(tid_swap_dec, NULL);
+	pthread_join(tid_swap_eq, NULL);
 	pthread_join(tid_mon, NULL);
 
 	return 0;
